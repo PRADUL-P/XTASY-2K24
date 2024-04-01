b87292f857ef34a026a3a9c897b22a78fb872241
PRADUL-P <pradul.p123@gmail.com>
2024-04-01 07:20:33 +0530

    data updated

--- src/database/datas.js
+++ src/database/datas.js
@@ -11,13 +11,13 @@ import Ub from "../assets/jazzboy.png";
 const Fday = [
   {
     title: "VEGETABLE CARVING ",
-    position: "OFF STAGE",
+    position: "COLLEGE LOBY",
     // icon:civilenggImage,
-   // fee: " 1500",
+    // fee: " 1500",
     iconBg: "#383E56",
     years: "REGISTRATION FEE : 100",
-    
-   
+
+
     points: [
       {
         header: "FACULTY IN CHARGES",
@@ -28,20 +28,20 @@ const Fday = [
         header: "STUDENT IN CHARGES",
         paragraph: "",
         date: "Abhiram P (S8 CSE) , Gopika N (S8 CSE) ",
-       },
-      
+      },
+
 
     ],
   },
   {
     title: "NADANPAATTU ",
-    position: "ON-STAGE ",
+    position: "SECOND STAGE",
     // icon:civilenggImage,
     //  fee: "fui",
     iconBg: "#383E56",
     years: "REGISTRATION FEE : 500",
-    
-   
+
+
     points: [
       {
         header: "FACULTY IN CHARGES",
@@ -52,19 +52,19 @@ const Fday = [
         header: "STUDENT IN CHARGES",
         paragraph: "",
         date: "Sreya Sajeevan (S4 CSE) ,Hridik (S4 EC) ",
-       },
-       
+      },
+
     ],
   },
   {
     title: "SPOT DANCE ",
-    position: "ON-STAGE ",
+    position: "MAIN STAGE",
     // icon:civilenggImage,
     //  fee: "fui",
     iconBg: "#383E56",
     years: "REGISTRATION FEE : 200",
-    
-   
+
+
     points: [
       {
         header: "FACULTY IN CHARGES",
@@ -75,31 +75,31 @@ const Fday = [
         header: "STUDENT IN CHARGES",
         paragraph: "",
         date: "Akash Pradeep (S6 ME) , Nandana Pradeep (S6 CE)  ",
-       },
-      ],
+      },
+    ],
+  },
+  {
+    title: "CINEMATIC DANCE ",
+    position: "MAIN STAGE ",
+    // icon:civilenggImage,
+    //  fee: "fui",
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : 600",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES",
+        paragraph: "",
+        date: "Ms. Sreedha Sreedhar , Mr. Rasma S ",
       },
       {
-        title: "CINEMATIC DANCE ",
-        position: "ON-STAGE ",
-        // icon:civilenggImage,
-        //  fee: "fui",
-        iconBg: "#383E56",
-        years: "REGISTRATION FEE : 600",
-        
-       
-        points: [
-          {
-            header: "FACULTY IN CHARGES",
-            paragraph: "",
-            date: "Ms. Sreedha Sreedhar , Mr. Rasma S ",
-          },
-          {
-            header: "STUDENT IN CHARGES",
-            paragraph: "",
-            date: "Anjeetha Suresh (S8 CSE) , Vishnu K (S8 ME)",
-           },
-          ],
-          },
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "Anjeetha Suresh (S8 CSE) , Vishnu K (S8 ME)",
+      },
+    ],
+  },
 
   // {
   //   title: "NAME",
@@ -120,206 +120,254 @@ const Fday = [
   //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
   //     // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
   //     // "Implementing responsive design and ensuring cross-browser compatibility.",
-      
+
   //   ],
   // },
- 
+
 
 ];
 const Sday = [
- { title: "VEGETABLE CARVING ",
-    position: "OFF STAGE",
+  {
+    title: "FACE PAINTING ",
+    position: "COLLEGE LOBY"
+    ,
     // icon:civilenggImage,
-   // fee: " 1500",
+    // fee: " 1500",
     iconBg: "#383E56",
     years: "REGISTRATION FEE : 100",
-    
-   
+
+
     points: [
       {
         header: "FACULTY IN CHARGES",
         paragraph: "",
-        date: "Ms. Roopa Balakrishnan , Ms. Jithika M",
+        date: "Dr. Arun Kumar Selvaraja Ms.Akshara K Anil",
       },
       {
         header: "STUDENT IN CHARGES",
         paragraph: "",
-        date: "Abhiram P (S8 CSE) , Gopika N (S8 CSE) ",
-       },
-      
+        date: "Nandana Das (S6 ME) Srikanth(S6 ME)",
+      },
+
 
     ],
   },
   {
-    title: "NADANPAATTU ",
-    position: "ON-STAGE ",
+    title: "MIMICRY ",
+    position: "SECOND STAGE"
+    ,
     // icon:civilenggImage,
     //  fee: "fui",
     iconBg: "#383E56",
-    years: "REGISTRATION FEE : 500",
-    
-   
+    years: "REGISTRATION FEE : 100",
+
+
     points: [
       {
         header: "FACULTY IN CHARGES",
         paragraph: "",
-        date: "Ms. Jisna P , Mr. Chandrajith E ",
+        date: "Ms. Jean Mary Jacob Ms.Saritha Narayanan",
       },
       {
         header: "STUDENT IN CHARGES",
         paragraph: "",
-        date: "Sreya Sajeevan (S4 CSE) ,Hridik (S4 EC) ",
-       },
-       
+        date: "Anurag K (S8 CSE)S arang K(S8 ME)",
+      },
+
     ],
   },
   {
-    title: "SPOT DANCE ",
-    position: "ON-STAGE ",
+    title: "SYNCHRONOUS DANCE ",
+    position: "MAIN STAGE",
     // icon:civilenggImage,
     //  fee: "fui",
     iconBg: "#383E56",
     years: "REGISTRATION FEE : 200",
-    
-   
+
+
     points: [
       {
         header: "FACULTY IN CHARGES",
         paragraph: "",
-        date: "Ms. Vaisakhi V K , Ms. Sreelakshmi Prasad ",
+        date: "Ms. Navitha V K Mr.Lipin K ",
       },
       {
         header: "STUDENT IN CHARGES",
         paragraph: "",
-        date: "Akash Pradeep (S6 ME) , Nandana Pradeep (S6 CE)  ",
-       },
-      ],
+        date: "Sariga Jayaraj (S8 CE) Abhijith K(S8 CE)  ",
+      },
+    ],
+  },
+  {
+    title: "RAMP WALK ",
+    position: "MAIN STAGE",
+    // icon:civilenggImage,
+    //  fee: "fui",
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : 1500",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES",
+        paragraph: "",
+        date: "Mr. Jubin Jose Ms.Deepthi K",
       },
       {
-        title: "CINEMATIC DANCE ",
-        position: "ON-STAGE ",
-        // icon:civilenggImage,
-        //  fee: "fui",
-        iconBg: "#383E56",
-        years: "REGISTRATION FEE : 600",
-        
-       
-        points: [
-          {
-            header: "FACULTY IN CHARGES",
-            paragraph: "",
-            date: "Ms. Sreedha Sreedhar , Mr. Rasma S ",
-          },
-          {
-            header: "STUDENT IN CHARGES",
-            paragraph: "",
-            date: "Anjeetha Suresh (S8 CSE) , Vishnu K (S8 ME)",
-           },
-          ],
-          },
- 
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "Ashwin K (S8 CSE) Sreerej M(S8 CSE)",
+      },
+    ],
+  },
+  {
+    title: "STAFF - SPECIAL PERFORMANCE ",
+    position: "MAIN STAGE",
+    // icon:civilenggImage,
+    //  fee: "fui",
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : ",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES ",
+        paragraph: "",
+        date: "",
+      },
+      {
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "",
+      },
+    ],
+  },
+
 ];
 const Tday = [
-  { title: "VEGETABLE CARVING ",
-     position: "OFF STAGE",
-     // icon:civilenggImage,
+  {
+    title: "MEHNDI DESIGN ",
+    position: "COLLEGE LOBY",
+    // icon:civilenggImage,
     // fee: " 1500",
-     iconBg: "#383E56",
-     years: "REGISTRATION FEE : 100",
-     
-    
-     points: [
-       {
-         header: "FACULTY IN CHARGES",
-         paragraph: "",
-         date: "Ms. Roopa Balakrishnan , Ms. Jithika M",
-       },
-       {
-         header: "STUDENT IN CHARGES",
-         paragraph: "",
-         date: "Abhiram P (S8 CSE) , Gopika N (S8 CSE) ",
-        },
-       
- 
-     ],
-   },
-   {
-     title: "NADANPAATTU ",
-     position: "ON-STAGE ",
-     // icon:civilenggImage,
-     //  fee: "fui",
-     iconBg: "#383E56",
-     years: "REGISTRATION FEE : 500",
-     
-    
-     points: [
-       {
-         header: "FACULTY IN CHARGES",
-         paragraph: "",
-         date: "Ms. Jisna P , Mr. Chandrajith E ",
-       },
-       {
-         header: "STUDENT IN CHARGES",
-         paragraph: "",
-         date: "Sreya Sajeevan (S4 CSE) ,Hridik (S4 EC) ",
-        },
-        
-     ],
-   },
-   {
-     title: "SPOT DANCE ",
-     position: "ON-STAGE ",
-     // icon:civilenggImage,
-     //  fee: "fui",
-     iconBg: "#383E56",
-     years: "REGISTRATION FEE : 200",
-     
-    
-     points: [
-       {
-         header: "FACULTY IN CHARGES",
-         paragraph: "",
-         date: "Ms. Vaisakhi V K , Ms. Sreelakshmi Prasad ",
-       },
-       {
-         header: "STUDENT IN CHARGES",
-         paragraph: "",
-         date: "Akash Pradeep (S6 ME) , Nandana Pradeep (S6 CE)  ",
-        },
-       ],
-       },
-       {
-         title: "CINEMATIC DANCE ",
-         position: "ON-STAGE ",
-         // icon:civilenggImage,
-         //  fee: "fui",
-         iconBg: "#383E56",
-         years: "REGISTRATION FEE : 600",
-         
-        
-         points: [
-           {
-             header: "FACULTY IN CHARGES",
-             paragraph: "",
-             date: "Ms. Sreedha Sreedhar , Mr. Rasma S ",
-           },
-           {
-             header: "STUDENT IN CHARGES",
-             paragraph: "",
-             date: "Anjeetha Suresh (S8 CSE) , Vishnu K (S8 ME)",
-            },
-           ],
-           },
-  
- ];
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : 100",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES",
+        paragraph: "",
+        date: "Mr. Madhu K Ms.Dinla O K",
+      },
+      {
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "Punnya Pradeep (S8 CSE) Shahana Shareen(S8 CSE)",
+      },
+
+
+    ],
+  },
+  {
+    title: "FANCY DRESS ",
+    position: "SECOND STAGE",
+    // icon:civilenggImage,
+    //  fee: "fui",
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : 100",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES",
+        paragraph: "",
+        date: "Ms. Manasa M Ms.Sreerekha K P",
+      },
+      {
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "Adhithya (S4 CE) Bithil(S6 ME)  ",
+      },
+
+    ],
+  },
+  {
+    title: "NOSTALGIA  ",
+    position: "MAIN STAGE",
+    // icon:civilenggImage,
+    //  fee: "fui",
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : 500",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES",
+        paragraph: "",
+        date: "Mr. Nivin V K Ms.Manu Thomas",
+      },
+      {
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "Aswanth C (S8 EC) Ashwanth Anilkumar(S8 CSE)",
+      },
+    ],
+  },
+  {
+    title: "PHOTOGRAPHY",
+    position: "ONLINE",
+    // icon:civilenggImage,
+    //  fee: "fui",
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : 50",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES",
+        paragraph: "",
+        date: "Mr. Aswin P Mr.Rahul S R",
+      },
+      {
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "Alen M (S2 EC) Yashika(S2 CS)",
+      },
+    ],
+  },
+  {
+    title: "CATCHING THE VIBE",
+    position: "ONLINE",
+    // icon:civilenggImage,
+    //  fee: "fui",
+    iconBg: "#383E56",
+    years: "REGISTRATION FEE : 50",
+
+
+    points: [
+      {
+        header: "FACULTY IN CHARGES",
+        paragraph: "",
+        date: "Mr. Nigil M Mr.Rahul S R ",
+      },
+      {
+        header: "STUDENT IN CHARGES",
+        paragraph: "",
+        date: "Abhay Rithik (S6 EC) Fahad (S8 CSE)",
+      },
+    ],
+  },
+
+];
 
 const projects = [
   {
     name: "Job Kurian",
-    name2:"singer",
+    name2: "singer",
     image: Webdes,
     source_code_link: "https://stthomaskannur.ac.in/stm2/home",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   // {
   //   name: "jazzboy",
@@ -328,16 +376,16 @@ const projects = [
   // },
   {
     name: "djsan",
-    name2:"DJ",
+    name2: "DJ",
     image: Ua,
     source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   {
     name: "Matadoria",
-    name2:"BAND",
+    name2: "BAND",
     image: Ui,
-    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   // {
   //   name: "Matadoria",
@@ -362,15 +410,15 @@ const projects = [
 const contact = [
   {
     title: "ASWIN P ",
-    role:"STAFF COORDINATOR",
-    contact:"+91 98094 21421",
-    whatsapp:"+9198094 21421",
+    role: "STAFF COORDINATOR",
+    contact: "+91 98094 21421",
+    whatsapp: "+9198094 21421",
   },
   {
     title: "NIGIL M",
-    role:"STAFF COORDINATOR",
-    contact:"+91 99462 51408",
-    whatsapp:"+9199462 51408",
+    role: "STAFF COORDINATOR",
+    contact: "+91 99462 51408",
+    whatsapp: "+9199462 51408",
   },
   // {
   //   title: "",
@@ -387,10 +435,10 @@ const contact = [
 const slider = [
   {
     name: "Job Kurian",
-    name2:"singer",
+    name2: "singer",
     image: Webdes,
     source_code_link: "https://stthomaskannur.ac.in/stm2/home",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   // {
   //   name: "jazzboy",
@@ -399,34 +447,34 @@ const slider = [
   // },
   {
     name: "djsan",
-    name2:"DJ",
+    name2: "DJ",
     image: Ua,
     source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   {
     name: "Matadoria",
-    name2:"BAND",
+    name2: "BAND",
     image: Ui,
-    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   {
     name: "Matadoria",
-    name2:"BAND",
+    name2: "BAND",
     image: Ui,
-    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   {
     name: "Matadoria",
-    name2:"BAND",
+    name2: "BAND",
     image: Ui,
-    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
   {
     name: "Matadoria",
-    name2:"BAND",
+    name2: "BAND",
     image: Ui,
-    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    source_code_link: "https://www.stthomaskannur.ac.in/stm2/IEDC/", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
   },
 ];
-export { Fday,Sday,Tday, projects,contact,slider };
+export { Fday, Sday, Tday, projects, contact, slider };
